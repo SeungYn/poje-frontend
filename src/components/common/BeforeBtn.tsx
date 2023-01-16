5aa98878e46139e9857f8c139fee4530014e9b31
SeungYn <kwls0407@gmail.com>
2023-01-17 02:24:08 +0900

    Feat : 이전으로 가는 버튼 공용콤포난토로 만듦 - 여러번 쓰일거 같아서 color랑 onClick을 받도록 함

---
+++ src/components/common/BeforeBtn.tsx
@@ -0,0 +1,25 @@
+import { MdNavigateBefore } from 'react-icons/md';
+import styled from 'styled-components';
+
+interface BeforeButtonType {
+  color: string;
+  onClick: () => void;
+}
+export default function BeforeBtn({ color, onClick }: BeforeButtonType) {
+  return (
+    <Button onClick={onClick} color={color}>
+      <MdNavigateBefore className='icon' />
+    </Button>
+  );
+}
+
+const Button = styled.button<{ color: string }>`
+  position: absolute;
+  left: 0.6rem;
+  top: 0.6rem;
+  color: ${({ color }) => color};
+
+  .icon {
+    font-size: ${({ theme }) => theme.iconSize};
+  }
+`;
