7c8750bbd40b67a1d8d85c6a897130fe84a41efc
SeungYn <kwls0407@gmail.com>
2023-02-27 10:54:19 +0900

    Fix : 루트 페이지 height:100% 적용

--- .history/src/pages/home/JobRoot_20230227104434.tsx
+++ .history/src/pages/home/JobRoot_20230227104434.tsx
@@ -13,7 +13,8 @@ const JobRootContainer = styled.section`
 	background-color: ${(props) => props.theme.bgSemiDarkColor};
 	height:100%;
 	width:100%;
-	
+	display:flex;
+	justify-content: center;
 
 `
 
