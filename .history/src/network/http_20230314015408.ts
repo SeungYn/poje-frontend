0a8dba1b195e12580068581c2dd7a02ddc4d7588
SeungYn <kwls0407@gmail.com>
2023-03-14 03:06:17 +0900

    Fix: 포트폴리오 프로젝트 추가 권한

--- .history/src/network/http_20230314015408.ts
+++ .history/src/network/http_20230314015408.ts
@@ -147,7 +147,7 @@ export default class Http {
     //     : 'http://15.164.128.201:8080';
     if (!Http.instance) {
       Http.instance = new Http(
-        process.env.REACT_APP_END_POINT!,
+        process.env.REACT_APP_END_POINT,
         new TokenStorage()
       );
     }
