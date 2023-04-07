1ae5d71816bffa999b84246166b12c902f02cc06
SeungYn <kwls0407@gmail.com>
2023-04-07 14:18:16 +0900

    Fix: 내정보 스크롤 버그 수정

--- src/components/member/MyPortfolioListContainer.tsx
+++ src/components/member/MyPortfolioListContainer.tsx
@@ -18,7 +18,6 @@ export default function MyPortfolioListContainer() {
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
